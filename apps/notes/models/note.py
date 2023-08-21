c2b49441f05240b2f9fc96dea1eb0c7fc3acd7b1
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2023-08-21 15:20:34 +0200

    first working version

---
+++ apps/notes/models/note.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Note(models.Model):
+    text_in = models.CharField(max_length=1000)
+    text_out = models.CharField(max_length=1000)
+
+    # passkey = models.CharField(max_length=100)
+
+    passkey = models.ForeignKey(
+        "SecretKey",
+        related_name="passkey",
+        on_delete=models.CASCADE,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.text_in
+
+    class Meta:
+        ordering = ["-modified_at", "text_in"]
